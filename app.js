1a333201a8d2a8fec70f5aa46351343e1e20e8af
penguin-number-123 <mumuji1723@gmail.com>
2024-04-15 07:03:02 +0800

    inb

---
+++ app.js
@@ -0,0 +1,26 @@
+
+const hiddenElements = document.querySelectorAll(".hidden");
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        console.log(entry)
+        if (entry.isIntersecting){
+            if(entry.target.classList.contains('hidden_left')){
+                entry.target.classList.add("show_left");
+            }
+            if(entry.target.classList.contains('hidden_right')){
+                entry.target.classList.add("show_right");
+            }
+            if(entry.target.classList.contains('hidden_up')){
+                entry.target.classList.add("show_up");
+            } 
+        }else{
+            entry.target.classList.remove("show_up");
+            entry.target.classList.remove("show_left");
+            entry.target.classList.remove("show_right");
+            entry.target.classList.remove("show");
+            
+            
+        }
+    })
+});
+hiddenElements.forEach((el)=>observer.observe(el));
