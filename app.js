4ae36df118c2c44e6cc56a243404e6f891466d7c
aquaponics-project <166908929+aquaponics-project@users.noreply.github.com>
2024-04-15 13:49:09 +0800

    AAAAAAAAAAAAAAAAAA

--- app.js
+++ app.js
@@ -13,11 +13,14 @@ const observer = new IntersectionObserver((entries)=>{
             if(entry.target.classList.contains('hidden_up')){
                 entry.target.classList.add("show_up");
             } 
+            if(entry.target.classList.contains('hidden_load')){
+                entry.target.classList.add("show_load");
+            }
         }else{
             entry.target.classList.remove("show_up");
             entry.target.classList.remove("show_left");
             entry.target.classList.remove("show_right");
-            entry.target.classList.remove("show");
+            entry.target.classList.remove("show_load");
             
             
         }
